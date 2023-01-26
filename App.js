a4d7b06c209589af2c7ba0f3a287c29c36e249f3
John Wesly Narala <nwesley@syncorsol.com>
2023-01-26 11:26:51 +0530

    class 3- laying foundation

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading1= React.createElement("h1", {
-    id: "title"
+// const heading1= React.createElement("h1", {
+//     id: "title"
     
-}, "Heading 1");
+// }, "Heading 1");
 
-const heading2= React.createElement("h2", {
-    id: "title"
+// const heading2= React.createElement("h2", {
+//     id: "title"
     
-}, "Heading 2");
+// }, "Heading 2");
 
-const container= React.createElement("div", {
-    id: "container"
+// const heading3= React.createElement("h3", {
+//     id: "title"
     
-}, [heading1, heading2]);
+// }, "Heading 3");
+
+// const container= React.createElement("div", {
+//     id: "container"
+    
+// }, [heading1, heading2, heading3]);
+
+
+// const heading= <div><h1>heading1</h1><h2>heading2</h2><h3>heading3</h3></div>;
+
+// heading= 
+// (<div>
+//     <h1>heading1</h1>
+//     <h2>heading2</h2>
+//     <h3>heading3</h3>
+//     </div>
+//     );
+
+
+// const Heading=() => {
+//    return(
+
+//    <div className= 'design'>
+//     {heading}
+//            <h1>heading1</h1>
+//             <h2>heading2</h2>
+//             <h3>heading3</h3>
+//             </div>
+//             );
+
+// };
+
+const Header= () => {
+
+return( 
+    <div>
+        <img src="images/logo1.jpg"></img>
+        <input type='search'>
+        </input>
+        <img src='images/user1.jpg'></img>
+
+    </div>
+);
+
+};
+
+
 
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
 
-root.render([container]);
+root.render(<Header/>);
 
